fe8c812e7e18b6620bcc69d27c69f2b4d1b1be32
Ivan Zakharchanka <3axap4eHko@gmail.com>
2019-06-14 17:42:44 -0400

    fixes

--- src/redux/create-store.js
+++ src/redux/create-store.js
@@ -1,23 +1,20 @@
 import {createStore, applyMiddleware, compose} from "redux";
 import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import {reducer} from './reducers';
 import {configureAPI} from '../api';
-import {createLogger} from 'redux-logger';
 
 const logger = createLogger();
 const api = configureAPI((...args) => store.dispatch(...args));
 const middlewares = [
   thunk.withExtraArgument(api),
   IS_DEV && logger, // eslint-disable-line
+  IS_DEV && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ].filter(Boolean);
 const store = createStore(
     // combinedReducers,
     reducer,
-    compose(
-        applyMiddleware(...middlewares),
-        (IS_DEV && window.__REDUX_DEVTOOLS_EXTENSION__) ? // eslint-disable-line
-          window.__REDUX_DEVTOOLS_EXTENSION__() : (a) => a
-    )
+  applyMiddleware(...middlewares),
 );
 
 export default store;
