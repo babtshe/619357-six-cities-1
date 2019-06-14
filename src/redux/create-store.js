f8b09ceb3e8a9534753222e7bc512db992be24e2
Alexander Babchenko <babtshe@yandex.com>
2019-06-14 11:22:28 +0300

    optimize devTools initialization, delete recompose - use redux compose instead, fix map layout error

--- src/redux/create-store.js
+++ src/redux/create-store.js
@@ -1,20 +1,15 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunk from 'redux-thunk';
-import {compose} from 'recompose';
 import {reducer} from './reducers';
 import {configureAPI} from '../api';
-
-const connectReduxDevtools = (...args) => {
-  const checkIfAvailable = () => {
-    return (typeof window !== undefined && window.__REDUX_DEVTOOLS_EXTENSION__);
-  };
-  return checkIfAvailable() ? [...args, window.__REDUX_DEVTOOLS_EXTENSION__()] : [...args];
-};
 
 const api = configureAPI((...args) => store.dispatch(...args));
 const store = createStore(
     reducer,
-    compose(...connectReduxDevtools(applyMiddleware(thunk.withExtraArgument(api))))
+    compose(applyMiddleware(thunk.withExtraArgument(api)),
+        (typeof window !== undefined && window.__REDUX_DEVTOOLS_EXTENSION__) ?
+          window.__REDUX_DEVTOOLS_EXTENSION__() :
+          (a) => a)
 );
 
 export {store};
